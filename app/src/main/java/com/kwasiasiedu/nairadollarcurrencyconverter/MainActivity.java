e3030e009b6ad4baf1b7966b8dfe2f51a839bfb8
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2017-07-14 01:24:04 +0100

    Pulling text from user input

--- app/src/main/java/com/kwasiasiedu/nairadollarcurrencyconverter/MainActivity.java
+++ app/src/main/java/com/kwasiasiedu/nairadollarcurrencyconverter/MainActivity.java
@@ -2,8 +2,21 @@ package com.kwasiasiedu.nairadollarcurrencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCash(View view) {
+
+        EditText myCashField = (EditText) findViewById(R.id.enterMoney);
+
+        //myCashField.getText().toString();
+
+        //int number = Integer.parseInt(myCashField);
+
+        }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

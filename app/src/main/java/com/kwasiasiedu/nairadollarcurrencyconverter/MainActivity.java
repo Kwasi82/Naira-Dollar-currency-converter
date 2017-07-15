12f21eadff19bba7d421461dbf2e0a814ea5cd5b
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2017-07-15 02:16:13 +0100

    Added actual conversion from Naira to Dollar. Comments included.

--- app/src/main/java/com/kwasiasiedu/nairadollarcurrencyconverter/MainActivity.java
+++ app/src/main/java/com/kwasiasiedu/nairadollarcurrencyconverter/MainActivity.java
@@ -10,9 +10,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void convert(View view) {
 
+        //Create an EditText variable which is equal to a field
         EditText nairaField = (EditText) findViewById(R.id.nairaField);
 
-        Log.i("nairaField", nairaField.getText().toString());
+        //Get the value of the field convert to a double (type of number)
+        Double nairaAmount = Double.parseDouble(nairaField.getText().toString());
+
+        //Convert naira to dollar using the conversion rate
+        Double dollarAmount = nairaAmount * 0.0032;
+
+        //display to the log the conversion
+        Log.i("dollarAmount", dollarAmount.toString());
+
+
 
         //myCashField.getText().toString();
 
